43508ae9b066c28eb73fe587b745266938f96f50
mmdzov <mzov939@gmail.com>
2022-01-28 18:33:35 +0330

    added api tests

--- src/components/home/home.spec.ts
+++ src/components/home/home.spec.ts
@@ -1,12 +1,26 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
 import HomeService from "./home.service";
+import request from "supertest";
+import app from "../../app";
 
-describe("home.service tests", () => {
+describe("service tests", () => {
   it("should to be string and must have hello world", () => {
     const homeService = new HomeService();
 
     const result = homeService.sayHello();
     expect(result).to.be.a("string").to.have.contain("Hello world");
+  });
+});
+
+describe("test API'S", () => {
+  it("test GET /", () => {
+    request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect("Content-Length", "13")
+      .end((err, res) => {
+        if (err) throw new Error(err);
+      });
   });
 });
