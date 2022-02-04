6ae07d8c8c3b440315b00215afc587f25464a8ed
mmdzov <mzov939@gmail.com>
2022-02-04 17:13:57 +0330

    Added playground for global process . such as functions & variables & etc

--- src/global/_needs.ts
+++ src/global/_needs.ts
@@ -3,17 +3,22 @@ import { TMiddleware, TRoute } from "../..";
 import routes from "./routes";
 import _attacher from "./_attacher";
 
-const _needs = ({
+const _needs = async ({
   app,
+  playground,
   router,
   middlewares,
 }: {
   app: Express;
+  playground?: () => Promise<void> | void;
   router: TRoute[];
   middlewares?: TMiddleware[];
 }) => {
-  middlewares?.map((middleware: TMiddleware) => app.use(_attacher(middleware)));
-  routes(app, router);
+  if (playground) await playground();
+  await middlewares?.map((middleware: TMiddleware) =>
+    app.use(_attacher(middleware))
+  );
+  await routes(app, router);
 };
 
 export default _needs;
